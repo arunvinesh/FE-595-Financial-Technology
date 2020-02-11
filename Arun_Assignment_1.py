329196cbcc7cc02cac9339632ec9dccf5ab2284b
arunvinesh <44319612+arunvinesh@users.noreply.github.com>
2020-02-10 20:04:43 -0500

    Updated the file name.

--- Arun_Assignment_1.py
+++ Arun_Assignment_1.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+#Plotting sine and cosine with Matplotlib and Python
+def plotsincos(st,sp,in):
+    x = np.arange(st, sp * np.pi, in)
+    y = np.sin(x)
+    z = np.cos(x)
 
-x= np.arange(0,4*np.pi,0.1)
-y= np.sin(x)
+    plt.plot(x, y, x, z)
+    plt.xlabel("x values from 0 to 4pi")  # string must be enclosed with quotes '  '
+    plt.ylabel("sin(x) and cos(x)")
+    plt.title("Plot of sin and cos from 0 to 4pi")
+    plt.legend(["sin(x)", "cos(x)"])  # legend entries as seperate strings in a list
+    plt.show()
 
-plt.plot(x,y)
-plt.show()
+plotsincos(0,4,0.1)
