82be11752985d0cecabb0ca7d12f0aa52a3d9a28
arunvinesh <44319612+arunvinesh@users.noreply.github.com>
2020-03-14 21:23:38 -0400

    Updated the file name.

---
+++ NLP_Data_Sorting.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import glob
+
+path = r"C:\Users\Arun vinesh\Documents\Arun\MS\Financial Services Analytics\FE-595-Financial Technology\Assignment\Assignment_3"
+all_files = glob.glob(path + "/*.csv")
+
+f = open("output_nlp_data_sorting.txt", "w")
+
+li = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, index_col=None, header=0)
+    li.append(df)
+
+frame = pd.concat(li, axis=0, ignore_index=True)
+
+print(frame, file=f)
